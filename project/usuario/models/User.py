c479afb2d4e850ede94ff878d3f386d1127b0cdd
matconi <silvadaxeira@gmail.com>
2023-09-04 10:37:44 -0300

    alter user manager padrão para adaptar ao projeto

--- project/usuario/models/User.py
+++ project/usuario/models/User.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.contrib.auth.models import UserManager
+from .manager.UserManager import CustomUserManager
 
 class User(AbstractUser):
     username = username_validator = None
@@ -9,6 +10,7 @@ class User(AbstractUser):
     last_name = models.CharField("Último Nome", max_length=120, blank=True, null=True, default=None)
     email = models.EmailField("Email", max_length=320, blank=False, null=False, unique=True)
     password = models.CharField("Senha", max_length=240)
+    phone = models.CharField("Celular", max_length=14)
     attempts = models.PositiveSmallIntegerField(
         "Tentativas", default=0,
         help_text="Tentativas de login com a senha incorreta. "
@@ -31,6 +33,8 @@ class User(AbstractUser):
 
     date_joined = models.DateTimeField("Entrou em", blank=True, null=True, default=None)
     last_login = models.DateTimeField("Último Login", blank=True, null=True, default=None)
+
+    objects = CustomUserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "password"]
