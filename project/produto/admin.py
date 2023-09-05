32e7a500d867b228706c4c30a30486c89363181e
matconi <silvadaxeira@gmail.com>
2023-09-05 20:31:19 -0300

    config inicial do model admin

--- project/produto/admin.py
+++ project/produto/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models.Category import Category
+from .models.Product import Product
+from .models.Variant import Variant
+from .models.Variation import Variation
+from .models.Unit import Unit
 
-# Register your models here.
+class UnitInline(admin.TabularInline):
+    model = Unit
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    inlines = [
+        UnitInline
+    ]
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Unit)
+admin.site.register(Variant)
+admin.site.register(Variation)
--- project/usuario/admin.py
+++ project/usuario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models.User import User
+from .models.Customer import Customer
+from .models.Payment import Payment
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Payment)
