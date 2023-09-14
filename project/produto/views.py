5113b14cb5965fb8a92ae192a5fcc76c33633907
matconi <silvadaxeira@gmail.com>
2023-09-14 10:47:09 -0300

    view e template para listagem

--- project/produto/views.py
+++ project/produto/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+from .domain.repositories import product_repository
+
+def index(request):
+    if request.method == 'GET':
+        units = product_repository.get_all()
+        return render(request, 'index.html', {"units": units})
