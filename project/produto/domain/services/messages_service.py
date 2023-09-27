947c8dbc39e0f28643ab38528b8675fc73b8a815
matconi <silvadaxeira@gmail.com>
2023-09-27 10:33:31 -0300

    sevice de mensagens e de carrinho

---
+++ project/produto/domain/services/messages_service.py
@@ -0,0 +1,51 @@
+from django.contrib import messages
+from django.utils.html import format_html
+from django.urls import reverse
+from produto.models.Unit import Unit
+
+def empty_avaliable(request, unit: Unit) -> None:
+    messages.warning(
+        request, format_html(                       
+        'Nenhuma unidade de "{}" disponível no momento. <a href="{}"> Meu Carrinho.</a>',
+        unit.name, reverse('produto:index')
+        )
+    )   
+
+def not_enough_to_add(request, quantity: int, unit: Unit, added: int) -> None:
+    messages.warning(
+        request,
+        f'Disponibilidade insuficiente para {quantity}x do produto "{unit.name}". ' 
+        f'Adicionamos {added}x no seu carrinho.'           
+    )
+
+def all_avaliable_warn(request, unit: Unit) -> None:
+    messages.warning(
+        request, format_html(                       
+            'Atualmente seu carrinho possui todas as unidades de "{}" disponíveis. '
+            'Recomendamos que finalize o pedido para garantir o produto.<a href="{}"> Meu Carrinho.</a>',
+            unit.name, reverse('produto:index')
+        )
+    )
+
+def added_to_cart(request, unit: Unit, quantity: int) -> None:
+    messages.success(
+        request, format_html(
+            'produto "{}" adicionado {}x no Carrinho.<a href="{}"> Confira.</a>',
+            unit.name, quantity, reverse('produto:index')
+        )   
+    )
+
+def not_enough_removed(request, unit: Unit) -> None:
+    messages.warning(
+        request, format_html(                       
+            'Nenhuma unidade de "{}" disponível no momento. O produto foi removido do carrinho<a href="{}"> Confira.</a>',
+            unit.name, reverse('produto:index')
+        )
+    )
+
+def over_avaliable(request, unit: Unit, over_avaliable: int) -> None:
+    messages.warning(
+        self.request,
+        f'Atualmente seu carrinho possui mais unidades de "{unit.name}" que o disponível. '
+        f'Reduzimos {over_avaliable}x do seu carrinho.'   
+    ) 
