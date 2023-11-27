a5573fdf9e3f7ada38c47eba70dc311eef6ac79f
matconi <silvadaxeira@gmail.com>
2023-11-27 13:13:37 -0300

    alterando forma de gerar slug

--- project/produto/models/Product.py
+++ project/produto/models/Product.py
@@ -4,14 +4,21 @@ from django.utils.text import slugify
 
 class Product(AbstractName):
     description = models.CharField("Descrição", max_length=400)
-    slug = models.SlugField("Link", max_length=120)
+    slug = models.SlugField(
+        "Link", max_length=120, unique=True, blank=True,
+        help_text='Deixe vazio para gerar automaticamente.'
+    )
 
     category = models.ForeignKey("Category", on_delete=models.RESTRICT, 
         verbose_name="Categoria"
     )
 
     def save(self):
-        self.slug = slugify(self.name)
+        if not self.slug:
+            self.slug = slugify(self.name)
+        else:
+            self.slug = slugify(self.slug)
+
         super(Product, self).save()
 
     def __str__(self) -> str:
